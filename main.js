56fbce76ea6f32d59540eb90f9be75630a30b689
AndreasRG <andreas@revesz-gudmann.dk>
2024-09-16 00:41:31 +0200

    Snapshot 1.14.2 Javascript testing

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-let age;
-age = 21;
-let name = "Andreas RG";
-console.log(age);
-console.log(name);
-let schoolName = `KEA`;
-const isStudent = true;
-console.log(schoolName);
-console.log(`Hello my name is ${name} and I am ${age} years old.`);
-console.log(name.length);
-console.log(schoolName + name);
-console.log(isStudent);
-console.log(typeof schoolName);
+/*
+const isUserLoggedIn = false;
+const accountType = 'standard';
+console.log(isUserLoggedIn && accountType === 'premium');
+console.log(accountType === 'vip' || accountType === 'premium');
+console.log(accountType === 'standard' || accountType === 'premium');
+if (isUserLoggedIn == true) {
+    console.log("Hello friend");
+} else {
+    console.log("Hello enemy");
+}
+
+for (let i = 30; i >= 10; i--) {
+    console.log(i);
+}
+
+const balance = 11000;
+if (balance > 10000) {
+    console.log('I am rich');
+}
+else {
+    console.log('I am poor');
+}
+
+
+const mood = "happy";
+
+if (mood == "happy") {
+    console.log("I am happy");
+}
+else if (mood == "angry") {
+    console.log("I am angry");
+}
+else {
+    console.log("I am confused");
+}
+
+for (let t = 0; t < 3; t++) {
+    console.log("Hello world");
+}
+*/
+let selectedItems = ['pc', 'bed', 'table'];
+console.log(selectedItems);
